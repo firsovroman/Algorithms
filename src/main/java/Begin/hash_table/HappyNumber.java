8247bf787439b3d3eaba5046f5addb2ccdc3f557
Roman Firsov <firsov_r@stdpr.ru>
2024-01-24 11:29:57 +0800

    .

---
+++ src/main/java/Begin/hash_table/HappyNumber.java
@@ -0,0 +1,12 @@
+package Begin.hash_table;
+
+public class HappyNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isHappy(int n) {
+        return false;
+    }
+}
