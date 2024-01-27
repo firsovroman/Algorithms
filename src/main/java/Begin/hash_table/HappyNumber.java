16e721d779992d0a77baba166bdaa475719329af
Roman Firsov <firsov_r@stdpr.ru>
2024-01-27 12:55:35 +0800

    .

--- src/main/java/Begin/hash_table/HappyNumber.java
+++ src/main/java/Begin/hash_table/HappyNumber.java
@@ -1,12 +1,35 @@
 package Begin.hash_table;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HappyNumber {
 
     public static void main(String[] args) {
+        int number1 = 19;
+        System.out.println("Число " + number1 + " является счастливым: " + isHappy(number1));
 
+        int number2 = 2;
+        System.out.println("Число " + number2 + " является счастливым: " + isHappy(number2));
     }
 
-    public boolean isHappy(int n) {
-        return false;
+    public static boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = calc(n);
+        }
+        return n == 1;
     }
+
+    public static int calc(int n) {
+        int sum = 0;
+        while (n != 0) {
+            int dig = n % 10;
+            sum += dig * dig;
+            n = n / 10;
+        }
+        return sum;
+    }
+
 }
