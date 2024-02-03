08804ba527127146d5a2c06711b6b38ec0453377
Roman Firsov <firsov_r@stdpr.ru>
2024-02-03 15:40:46 +0800

    .

---
+++ src/main/java/Begin/hash_table/GroupAnagrams.java
@@ -0,0 +1,38 @@
+package Begin.hash_table;
+
+import java.util.*;
+
+public class GroupAnagrams {
+
+    public static void main(String[] args) {
+
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if(strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> anagramGroups = new HashMap<>();
+        for (String s : strs) {
+            char[] chars =  s.toCharArray();
+            Arrays.sort(chars);
+            String stroka = new String(chars);
+
+            if(anagramGroups.containsKey(stroka)) {
+                List<String> list = anagramGroups.get(stroka);
+                list.add(s);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                anagramGroups.put(stroka,list);
+            }
+        }
+
+        return new ArrayList<>(anagramGroups.values());
+    }
+
+}
