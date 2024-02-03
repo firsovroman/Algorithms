36e0d238b4b6aab8d6adc1b32b1a82a383b7e258
Roman Firsov <firsov_r@stdpr.ru>
2024-02-03 17:58:04 +0800

    .

---
+++ src/main/java/Begin/hash_table/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,50 @@
+package Begin.hash_table;
+
+import java.util.*;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+
+    public static void main(String[] args) {
+        String s0 = " ";
+        System.out.println(lengthOfLongestSubstring(s0)); // 1
+
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s)); // 3
+
+        String s2 = "bbbbb";
+        System.out.println(lengthOfLongestSubstring(s2)); // 1
+
+        String s3 = "pwwkew";
+        System.out.println(lengthOfLongestSubstring(s3)); // 3
+
+        String s4 = "dvdf";
+        System.out.println(lengthOfLongestSubstring(s4)); // 3
+
+    }
+
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        int result = 0;
+        int start = 0;
+        for (int end = 0; end < s.length(); end++) {
+            char curChar = s.charAt(end);
+
+            if (map.containsKey(curChar)) {
+                start = Math.max(start, map.get(curChar) + 1);
+            }
+
+            map.put(curChar, end);
+            result = Math.max(result, end - start + 1);
+        }
+        return result;
+    }
+
+
+}
