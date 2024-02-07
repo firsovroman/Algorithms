d54e14412284bdaa71ca50f1993ab53f624d9191
Roman Firsov <firsov_r@stdpr.ru>
2024-02-07 12:06:02 +0800

    .

--- src/main/java/Begin/QueueAndStack/ValidParentheses.java
+++ src/main/java/Begin/QueueAndStack/ValidParentheses.java
@@ -25,17 +25,15 @@ public class ValidParentheses {
 
     public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
-                char pop = stack.pop();
-                if (ch == ')' && pop != '(' || ch == ']' && pop != '[' || ch == '}' && pop != '{') {
-                    return false;
-                }
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
             }
         }
         return stack.isEmpty();
