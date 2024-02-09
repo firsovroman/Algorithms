51bfd5d666adf0b18892305b48371d48ece1c737
Roman Firsov <firsov_r@stdpr.ru>
2024-02-09 11:54:03 +0800

    .

---
+++ src/main/java/Begin/QueueAndStack/ImplementQueueUsingStacks.java
@@ -0,0 +1,37 @@
+package Begin.QueueAndStack;
+
+public class ImplementQueueUsingStacks {
+
+    public static void main(String[] args) {
+        MyQueue obj = new MyQueue();
+        obj.push(1);
+        int param_2 = obj.pop();
+        int param_3 = obj.peek();
+        boolean param_4 = obj.empty();
+    }
+
+    static class MyQueue {
+
+        public MyQueue() {
+
+        }
+
+        public void push(int x) {
+
+        }
+
+        public int pop() {
+            return 0;
+        }
+
+        public int peek() {
+            return 0;
+        }
+
+        public boolean empty() {
+            return true;
+        }
+    }
+}
+
+
