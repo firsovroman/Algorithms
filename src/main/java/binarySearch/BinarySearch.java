bd0fb450a8a1e7d730f4d8cdad958a07cbefc9f9
Roman Firsov <firsov_r@stdpr.ru>
2024-02-21 09:49:42 +0800

    .

---
+++ src/main/java/binarySearch/BinarySearch.java
@@ -0,0 +1,36 @@
+package binarySearch;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+
+        System.out.println(search(nums, target)); // 4
+    }
+
+    public static int search(int[] nums, int target) {
+        int high = nums.length - 1;
+        int low = 0;
+        return binarySearch(nums, target, low, high);
+    }
+
+    public static int binarySearch(int[] sortedArray, int key, int low, int high) {
+        int index = -1;
+
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            if (sortedArray[mid] > key) {
+                high = mid - 1;
+            } else if (sortedArray[mid] < key) {
+                low = mid + 1;
+            } else if (sortedArray[mid] == key) {
+                index = mid;
+                break;
+            }
+        }
+
+        return index;
+    }
+}
