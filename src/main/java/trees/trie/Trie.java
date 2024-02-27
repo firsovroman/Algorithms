40cdca8c26d8fb2c694477c5dd2c452a26283acc
Roman Firsov <firsov_r@stdpr.ru>
2024-02-27 09:52:44 +0800

    .

--- src/main/java/trees/trie/Trie.java
+++ src/main/java/trees/trie/Trie.java
@@ -3,19 +3,55 @@ package trees.trie;
 //  Implement Trie (Prefix Tree)
 class Trie {
 
-    public Trie() {
+    public static void main(String[] args) {
 
+    }
+
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
     }
 
     public void insert(String word) {
-
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                node.children[ch - 'a'] = new TrieNode();
+            }
+            node = node.children[ch - 'a'];
+        }
+        node.isEndOfWord = true;
     }
 
     public boolean search(String word) {
-        return false;
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEndOfWord;
     }
 
     public boolean startsWith(String prefix) {
-        return false;
+        return searchPrefix(prefix) != null;
+    }
+
+    private TrieNode searchPrefix(String prefix) {
+        TrieNode node = root;
+        for (char ch : prefix.toCharArray()) {
+            if (node.children[ch - 'a'] == null) {
+                return null;
+            }
+            node = node.children[ch - 'a'];
+        }
+        return node;
+    }
+
+    class TrieNode {
+        TrieNode[] children;
+        boolean isEndOfWord;
+
+        public TrieNode() {
+            children = new TrieNode[26];
+            isEndOfWord = false;
+        }
     }
 }
+
