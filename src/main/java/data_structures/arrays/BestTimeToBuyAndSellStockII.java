88b043cde7fe8065c573b6d0012f6782ac77d93c
Roman Firsov <firsov_r@stdpr.ru>
2024-02-29 09:20:24 +0800

    .

--- src/main/java/data_structures/arrays/BestTimeToBuyAndSellStockII.java
+++ src/main/java/data_structures/arrays/BestTimeToBuyAndSellStockII.java
@@ -6,9 +6,9 @@ public class BestTimeToBuyAndSellStockII {
 
     public static void main(String[] args) {
 
-        int[] prices1 = {7,1,5,3,6,4};
-        int[] prices2 = {1,2,3,4,5};
-        int[] prices3 = {7,6,4,3,1};
+        int[] prices1 = {7, 1, 5, 3, 6, 4};
+        int[] prices2 = {1, 2, 3, 4, 5};
+        int[] prices3 = {7, 6, 4, 3, 1};
         System.out.println(maxProfit(prices1)); // 7
         System.out.println(maxProfit(prices2)); // 4
         System.out.println(maxProfit(prices3)); // 0
@@ -16,12 +16,12 @@ public class BestTimeToBuyAndSellStockII {
     }
 
     public static int maxProfit(int[] arr) {
-        int i = 1, j = 0, sum = 0;
+        int i = 1;
+        int j = 0;
+        int sum = 0;
 
         while (i < arr.length) {
-            if (arr[i] <= arr[j]) {
-                // do nothing
-            } else {
+            if (arr[i] > arr[j]) {
                 int diff = arr[i] - arr[j];
                 sum += diff;
             }
