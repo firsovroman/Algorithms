49c587947a8288c235cb493ecea1278f0e69bd7d
Roman Firsov <firsov_r@stdpr.ru>
2024-03-08 18:52:36 +0800

    .

---
+++ src/main/java/trees/binary_tree/SymmetricTree.java
@@ -0,0 +1,60 @@
+package trees.binary_tree;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SymmetricTree {
+
+    public static void main(String[] args) {
+        /*
+                     1
+                   /   \
+                  2     2
+                 / \   / \
+                4   3  3  4
+        */
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+
+        root.right.left = new TreeNode(3);
+        root.right.right = new TreeNode(4);
+
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(3);
+
+
+        System.out.println(isSymmetric(root));
+    }
+
+    public static boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+
+            if (t1.val != t2.val) {
+                return false;
+            }
+            queue.add(t1.left);
+            queue.add(t2.right);
+            queue.add(t1.right);
+            queue.add(t2.left);
+        }
+
+        return true;
+    }
+
+}
