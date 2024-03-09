1c2116bb5289056f1b2f06dda5f8e41c391559d5
Roman Firsov <firsov_r@stdpr.ru>
2024-03-09 16:39:14 +0800

    .

--- src/main/java/trees/binary_tree/ConvertSortedArrayToBinarySearchTree.java
+++ src/main/java/trees/binary_tree/ConvertSortedArrayToBinarySearchTree.java
@@ -13,11 +13,10 @@ public class ConvertSortedArrayToBinarySearchTree {
         if (nums == null || nums.length == 0) {
             return null;
         }
-
-        return constructBST(nums, 0, nums.length - 1);
+        return sortedArrayToBSTRec(nums, 0, nums.length - 1);
     }
 
-    private static TreeNode constructBST(int[] nums, int left, int right) {
+    public static TreeNode sortedArrayToBSTRec(int[] nums, int left, int right) {
         if (left > right) {
             return null;
         }
@@ -25,9 +24,8 @@ public class ConvertSortedArrayToBinarySearchTree {
         int mid = left + (right - left) / 2;
         TreeNode root = new TreeNode(nums[mid]);
 
-        root.left = constructBST(nums, left, mid - 1);
-        root.right = constructBST(nums, mid + 1, right);
-
+        root.left = sortedArrayToBSTRec(nums, left, mid - 1);
+        root.right = sortedArrayToBSTRec(nums, mid + 1, right);
         return root;
     }
 
