29f787c229191e2101a4549e308a35bbe53f0f51
Roman Firsov <firsov_r@stdpr.ru>
2024-03-13 06:36:40 +0800

    .

--- src/main/java/math/RomanToInteger.java
+++ src/main/java/math/RomanToInteger.java
@@ -1,15 +1,38 @@
 package math;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanToInteger {
 
-
     public static void main(String[] args) {
-
+        System.out.println(romanToInt("MCMXCIV")); // 1994
     }
 
 
-    public int romanToInt(String s) {
-        return 0;
+    public static int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        char[] arr = s.toCharArray();
+        int sum = 0;
+        int prevValue = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int curValue = map.get(arr[i]);
+            if (curValue < prevValue) {
+                sum -= curValue;
+            } else  {
+                sum += curValue;
+            }
+            prevValue = curValue;
+        }
+        return sum;
     }
 
 }
